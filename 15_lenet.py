ca09f5eeb5422194229fe0a2795c49f6659e7f88
yanqiangmiffy <1185918903@qq.com>
2020-02-13 11:31:30 +0800

    LENET

---
+++ 15_lenet.py
@@ -0,0 +1,123 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@version:
+author:yanqiang
+@time: 2019/04/09
+@file: main.py
+@description: LeNet
+"""
+# LeNet包含输入层在内共有八层，每一层都包含多个权重。C层代表卷积层，通过卷积操作，可以使原信号特征增强，并降低噪音。S层是一个池化层，
+# 利用图像局部相关性的原理，对图像进行子抽样，可以减少数据处理量，同时也保留了一定的有用信息。
+# https://www.jianshu.com/p/d4654f12f523
+
+# 卷积神经网络(LeNet)
+
+import os
+import time
+import torch
+from torch import nn, optim
+
+import sys
+
+sys.path.append('.')
+import d2lzh_pytorch as d2l
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print(torch.__version__)
+print(device)
+
+
+# LeNet模型
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 6, 5),  # in_channels,out_channels,kernel_size
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2),  # kernel_size, stride
+            nn.Conv2d(6, 16, 5),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16 * 4 * 4, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, img):
+        feature = self.conv(img),
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+
+net = LeNet()
+print(net)
+
+# 获取训练数据
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用。该函数将被逐步改进：它的完整实现将在“图像增广”一节中描述
+def evaluate_accuracy(data_iter, net, device=None):
+    """
+
+    :param data_iter: 数据迭代器
+    :param net: 模型
+    :param device:cpu or gpu
+    :return:
+    """
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, torch.nn.Module):
+                net.eval()  # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train()  # 改回训练模式
+            else:  # 自定义的模型, 3.13节之后不会用到, 不考虑GPU
+                if ('is_training' in net.__code__.co_varnames):  # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
+    return acc_sum / n
+
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+
+
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
