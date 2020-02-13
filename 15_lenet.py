b32373d30b650ac8e479604795005798ea42c66e
yanqiangmiffy <1185918903@qq.com>
2020-02-13 11:33:51 +0800

    LENET

--- 15_lenet.py
+++ 15_lenet.py
@@ -35,15 +35,15 @@ class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 6, 5),  # in_channels,out_channels,kernel_size
+            nn.Conv2d(1, 6, 5), # in_channels, out_channels, kernel_size
             nn.Sigmoid(),
-            nn.MaxPool2d(2, 2),  # kernel_size, stride
+            nn.MaxPool2d(2, 2), # kernel_size, stride
             nn.Conv2d(6, 16, 5),
             nn.Sigmoid(),
             nn.MaxPool2d(2, 2)
         )
         self.fc = nn.Sequential(
-            nn.Linear(16 * 4 * 4, 120),
+            nn.Linear(16*4*4, 120),
             nn.Sigmoid(),
             nn.Linear(120, 84),
             nn.Sigmoid(),
@@ -51,7 +51,7 @@ class LeNet(nn.Module):
         )
 
     def forward(self, img):
-        feature = self.conv(img),
+        feature = self.conv(img)
         output = self.fc(feature.view(img.shape[0], -1))
         return output
 
