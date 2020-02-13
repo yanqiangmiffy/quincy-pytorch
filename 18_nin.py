e782c62da28c7e18b657c01e63da28bb08e8f873
yanqiangmiffy <1185918903@qq.com>
2020-02-13 12:00:21 +0800

    nin

---
+++ 18_nin.py
@@ -0,0 +1,71 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@version:
+author:yanqiang
+@time: 2019/04/09
+@file: main.py
+@description: Network In Network
+"""
+# *提出了mlpconv代替普通的卷积操作，从而来提升抽象能力
+# *提出了globalaveragepooling，从而将CNN与FC有机的结合在一起，使得整个网络结构的可解释性更加强，效果也更佳
+# https://blog.csdn.net/whz1861/article/details/78075290
+
+# 网络中的网络
+import time
+import torch
+from torch import nn, optim
+
+import sys
+sys.path.append("..")
+import d2lzh_pytorch as d2l
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print(torch.__version__)
+print(device)
+
+
+
+def nin_block(in_channels, out_channels, kernel_size, stride, padding):
+    blk = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+                        nn.ReLU(),
+                        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+                        nn.ReLU(),
+                        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+                        nn.ReLU())
+    return blk
+
+
+net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, stride=4, padding=0),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nin_block(96, 256, kernel_size=5, stride=1, padding=2),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nin_block(256, 384, kernel_size=3, stride=1, padding=1),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Dropout(0.5),
+    # 标签类别数是10
+    nin_block(384, 10, kernel_size=3, stride=1, padding=1),
+    # 全局平均池化层可通过将窗口形状设置成输入的高和宽实现
+    nn.AvgPool2d(kernel_size=5),
+    # 将四维的输出转成二维的输出，其形状为(批量大小, 10)
+    d2l.FlattenLayer())
+
+
+
+X = torch.rand(1, 1, 224, 224)
+
+for name, blk in net.named_children():
+    X = blk(X)
+    print(name, 'output shape: ', X.shape)
+
+
+# 获取数据和训练模型
+
+batch_size = 128
+# 如出现“out of memory”的报错信息，可减小batch_size或resize
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+
+lr, num_epochs = 0.002, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+d2l.train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
